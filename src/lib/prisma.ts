056b3fd54016575f2124c6a213efe76964b3f12a
chirag-gupta1 <chirag.gupta7374@gmail.com>
2025-12-15 05:01:35 -0600

    Debugging app.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,13 @@
-const { PrismaClient } = require("@prisma/client");
-import { PrismaPg } from "@prisma/adapter-pg";   // <-- adapter package
-import pg from "pg";                             // <-- node-postgres
-import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const createPrismaClient = () => {
-  const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-  const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg(pool);
 
-  return new PrismaClient({
-    adapter,
-  });
-};
+const prisma = new PrismaClient({ adapter });
 
-const prisma = createPrismaClient();
 export default prisma;
